c7049455b1fc9bd25c92605aa67037ec54dc7021
Victor <vrcjunes@inf.ufpel.edu.br>
2017-07-11 10:23:35 -0300

    first commit

---
+++ typecheck.hs
@@ -0,0 +1,18 @@
+data E = Num Int | Soma E E | Mult E E
+	deriving (Eq,Show)
+
+data T = INT
+	deriving (Eq,Show)
+
+typeCheck:: E -> T
+typeCheck (Num n) = INT
+typeCheck (Soma e1 e2) =
+				| typeCheck e1 == INT = typeCheck e2
+				| otherwise		      = error (show e1 ++ "Should have type INT")
+typeCheck (Mult e1 e2) =
+				| typeCheck e1 == INT = typeCheck e2
+				| otherwise			  = error (show e1 ++ "Should have type INT")
+
+
+prog1:: E
+prog1 = Soma (Soma (Num 4) (Num 5)) (Num 12)
